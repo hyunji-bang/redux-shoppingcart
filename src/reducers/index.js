42443223241b8b867cb66aea7c8d92a1f092e7a5
방현지 <stella@rsquare.co.kr>
2018-02-27 16:07:51 +0900

    add redux

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import * as types from '../actions/ActionTypes';
+
+// 초기 상태를 정의합니다
+const initialState = {
+};
+
+function cart(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default cart;
