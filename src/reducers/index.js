44cf18b469f470428905182b597f364d7d12e47e
방현지 <stella@rsquare.co.kr>
2018-03-22 19:41:55 +0900

    Correct "My wrong answer" and Comment about it to arrange my logic

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -43,17 +43,18 @@ function cart(state = initialState.cart_list, action) {
                     newState.push(firstItem);
                     return newState;
                 } else {
-                    const doesntExist = newState.every((cartObj) => {
-                        return cartObj.name !== action.item.name
+                    const doesntExist = newState.every((cartObj) => { // 모든 요소가 true가 되는지 체크
+                        return cartObj.name !== action.item.name // 클릭한 값이 state의 배열 값들과 모두같지 않다면 true => 클릭한거 push
+                                                                 // 즉, 하나라도 같은 값이 있다면 false => 해당 인덱스에 +1
                     })
 
-                    const firstItem = {...action.item, amount: 1};
+                    const pushItem = {...action.item, amount: 1}; // 푸쉬해줄 클릭한 값
 
                     if ( doesntExist ) { // 기존 배열(카트)에 없는 값을 클릭했을 경우
-                        newState.push(firstItem);
+                        newState.push(pushItem);
                     } else { // 있는 값을 클릭했을 경우 -> 해당위치에 +1
                         newState.map((cartObj, index) => {
-                            if (cartObj.name === action.item.name) {
+                            if (cartObj.name === action.item.name) { // 기존에 있던 else는 필요없다. else일 경우 모두다 pass
                                 newState[index] = {
                                     ...newState[index],
                                     amount: cartObj.amount + 1
@@ -67,31 +68,23 @@ function cart(state = initialState.cart_list, action) {
 
                     // newState.map((cartObj, index) => {// cart_list 배열의 obj들을 하나하나 돌면서 있는건지 검증
                     //     if (cartObj.name === action.item.name) { // 기존에 있는 제품이면, 그 제품을 바꿔주기
-                    //         console.log('* 같 다 ----------');
-                    //         console.log('* index', index);
-                    //         console.log('*cartObj.name: ', cartObj.name, ' / action.item.name : ', action.item.name)
                     //         const addedItem = {...action.item, amount: cartObj.amount + 1};
                     //         console.log('애프터 >>>>>> addedItem', addedItem);
                     //         newState[index] = {
                     //             ...cartObj,
                     //             amount: addedItem.amount
                     //         }
-                    //     } else { // 기존에 없는 제품이 들어왔을 경우
-                    //         // 1. 그냥 건너뛰던지
-                    //         // 2. 아예 없으니까 추가해줘야하는 제품인지.
-                    //         console.log('* 같 지 않 다 ----------');
-                    //         console.log('*cartObj.name : ', cartObj.name, ' / action.item.name : ', action.item.name)
                     //
+                    //     } else { // 기존에 없는 제품이 들어왔을 경우 -> X 차례대로 map이 도는 와중에 클릭한 값이 아닐 경우
+                    //         // 1. 그냥 건너뛰던지 -> 이게 맞음 OOOOOO 건너뛰어야함
+                    //
+                    //         // 2. 아예 없으니까 추가해줘야하는 제품인지 -> 이건 애초에 먼저 다른 조건문으로 체크했어야 할 사항.
                     //         // console.log('<<<------아무것도 없으니까 푸쉬----->>>');
                     //         // const firstItem = {...action.item, amount: 1};
                     //         // newState.push(firstItem);
                     //     }
                     // })
-                    // console.log('newState 리턴!!!!!!!!!!!!!', newState)
                     // return newState
-                    //
-                    // //console.log('after>> newState: ', newState)
-                    // //return newState
                 }
             } else { // 수량이 없는 걸 클릭했을 경우
                 return state;
